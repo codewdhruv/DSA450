d531b545bf982d5ff7d21fa43f1c9d3355fc822e
Dhruba Jyoti Chakraborty <61685945+dhrubajyoti89@users.noreply.github.com>
2021-08-20 14:17:54 +0530

    Create negtobeg.cpp

---
+++ arrays/negtobeg.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#partition process of quick sort 
+
+int rearrange(int arr[], int n){
+  int j=0;
+  for(int i=0;i<n;i++)
+  {
+    if(arr[i]<0){
+      if(i!=j)
+        swap(a[i],a[j]);
+      j++;
+    }
+  }
+}
+
+int printArray(int arr[], int n){
+  for(int=0;i<n;i++)
+   cout<<arr[i];
+}
+
+
+int main(){
+   int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    rearrange(arr, n);
+    printArray(arr, n);
+    return 0;
+}
