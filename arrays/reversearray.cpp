c066709926c00e835ca037c7cce8b704b80cd9fa
Dhruba Jyoti Chakraborty <61685945+dhrubajyoti89@users.noreply.github.com>
2021-08-20 11:57:26 +0530

    Update reversearray.cpp

--- arrays/reversearray.cpp
+++ arrays/reversearray.cpp
@@ -1,1 +1,14 @@
 
+string reverseWord(string str){
+    
+  //Your code here
+  int i=0;
+  int j = str.length()-1;
+char temp;
+for(;i<=j;i++,j--){
+  temp=str[i];
+  str[i] = str[j];
+  str[j] = temp;
+  }
+  return str;
+}
