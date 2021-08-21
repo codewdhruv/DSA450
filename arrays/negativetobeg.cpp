90417d1328d192e77dc36f29a78cb55035102fc4
Dhruba Jyoti Chakraborty <61685945+dhrubajyoti89@users.noreply.github.com>
2021-08-21 09:43:35 +0530

    Create negativetobeg.cpp

---
+++ arrays/negativetobeg.cpp
@@ -0,0 +1,14 @@
+# TWO POINTER METHOD SOLUTION
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int arr[] = {-12, 11, -13, -5,
+               6, -7, 5, -3, 11};
+  int n= sizeof(arr)/sizeofarr[0];
+  
+  shiftall(arr,0,arr_size-1);
+  display(arr,arr_size-1);
+  return 0;
+}
